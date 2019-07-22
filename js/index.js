22f8c43d0e65c07aa99201dca7f0ae9a97bc90b7
bobbidigi <bobbidigi34@gmail.com>
2019-07-22 13:44:48 -0400

    mouseover and keydown

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+
+
+////// button animation triggered on mouseover
+const cPick = document.querySelector(".content-pick");
+
+cPick.addEventListener('mouseover', (event) => {
+    console.log(event.target)
+    if(event.target.className === "btn"){
+        event.target.classList.add('btnAnimation');
+        setTimeout(function () {
+            event.target.classList.remove("btnAnimation");
+        }, 3000);
+    }
+})
+
+
+///body animation triggered by Keydown
+window.addEventListener('keydown', (event) => {
+    console.log(event.target)
+    event.target.classList.add('bodyAnimation');
+    setTimeout(function () {
+        event.target.classList.remove("bodyAnimation");
+    }, 3000);
+})
+
+
