9d04c2df8331610bb4128d4c36ec3c27417a1c70
bobbidigi <bobbidigi34@gmail.com>
2019-08-09 15:08:56 -0400

    add: travelers, will travel and come back

---
+++ stretch-assignment/index.js
@@ -0,0 +1,49 @@
+let blocks = document.querySelector('.blocks');
+let margin = 10;
+let traveling; 
+// console.log(blocks)
+
+
+blocks.addEventListener('mousedown', (e)=>{
+  traveling = true;
+  
+  let blockMargin = 10;
+    timerAdd();
+    function timerAdd() {
+      m = setTimeout(addMargin, 10);
+    }
+    function timerSubtract() {
+      m = setTimeout(subMargin, 10);
+    }
+    function subMargin(){
+      if(blockMargin > 10){
+        blockMargin--;
+        e.target.style.marginLeft = `${blockMargin}px`;
+        timerSubtract();
+      }
+      
+    }
+    function addMargin() {
+      if(traveling){
+        blockMargin++;
+        e.target.style.marginLeft = `${blockMargin}px`;
+        timerAdd();
+      }else{
+        subMargin();
+      }
+      
+    }
+})
+
+blocks.addEventListener('mouseup', (e) =>{
+  traveling = false;
+
+})
+
+
+
+
+
+
+
+
